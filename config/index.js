6f34c284dd83b28822ea8e1bca7cabf554bdb19b
YunguoMu <109849552+YunguoMu@users.noreply.github.com>
2022-09-06 09:13:47 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ export const config = {
   // 公众号APP_ID 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: "wx2e030260b92cfd29",
+  // APP_ID: "",
 
   // 公众号APP_SECRET 
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: "70de808ce6f57279700d44bdcf57c022",
+  // APP_SECRET: "",
 
   /**
    * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
@@ -24,7 +24,7 @@ export const config = {
    *    // 你想对他发送的模板消息的模板ID
    *    useTemplateId: "kDkFt-1OuJMxkznKVqsPUcLRFME0Q61XdI_Ly9BgpT4",
    *    // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-   *    openUrl: "https://wangxinleo.cn"
+   *    openUrl: "https://wangxinleo.cnhttps://www.researchgate.net/profile/Yunguo-Mu"
    *   }, 
    * ]
    */
